3d7fa76485be48538eaf9546da21d42da0dc6cee
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-23 23:46:28 +0530

    Updated settings.py file

--- Calculator/Calculator/settings.py
+++ Calculator/Calculator/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'Calculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'ArithmeticApp','static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
--- Calculator/Calculator/urls.py
+++ Calculator/Calculator/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
+	path('',include('ArithmeticApp.urls')),
+	path('ArithmeticCalc',include('ArithmeticApp.urls')),
     path('admin/', admin.site.urls),
 ]
