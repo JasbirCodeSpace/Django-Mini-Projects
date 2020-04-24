34096c2e62c71f640580a1a424fa8d077119aa02
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-24 18:01:16 +0530

    Updated ArithmeticApp views

--- Calculator/ArithmeticApp/views.py
+++ Calculator/ArithmeticApp/views.py
@@ -4,9 +4,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def Arithmetic(request):
-	input1 = int(request.POST['input1'])
-	input2 = int(request.POST['input2'])
-	opeartor = int(request.POST['opeartor'])
-	return format(eval(input1+opeartor+input2),'.2f')
+	input1 = request.POST['input1']
+	input2 = request.POST['input2']
+	oper = request.POST['oper']
+	print(request.POST)
+	return HttpResponse(format(eval(input1+oper+input2),'.2f'))
 def home(request):
 	return render(request,'index.html')
