d9e1e5cf7b59671700bca43521c85feabb599098
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-25 14:11:13 +0530

    Updated urls.py

--- AutomatedMails/AutomatedMails/urls.py
+++ AutomatedMails/AutomatedMails/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-	path('SimpleMail',include('SimpleMail.urls')),
+	path('',include('SimpleMail.urls')),
+	path('SendMail',include('SimpleMail.urls')),
     path('admin/', admin.site.urls),
 ]
