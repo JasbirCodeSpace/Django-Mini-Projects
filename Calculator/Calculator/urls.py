3c66f22513d8bc28024aeed8406f721dd2f1990b
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-26 20:06:36 +0530

    Updated urls.py

--- Calculator/Calculator/urls.py
+++ Calculator/Calculator/urls.py
@@ -18,6 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
 	path('',include('ArithmeticApp.urls')),
-	path('ArithmeticCalc',include('ArithmeticApp.urls')),
     path('admin/', admin.site.urls),
 ]
