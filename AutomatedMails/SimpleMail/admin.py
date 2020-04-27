c71a0f10bae5d135641b3cd08b1d89f73163b050
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-27 23:03:04 +0530

    Updated admin, apps and urls files

--- AutomatedMails/SimpleMail/admin.py
+++ AutomatedMails/SimpleMail/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ContactDetail
 
 # Register your models here.
+admin.site.register(ContactDetail)
+
--- AutomatedMails/SimpleMail/apps.py
+++ AutomatedMails/SimpleMail/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class SimplemailConfig(AppConfig):
+class SimpleMailConfig(AppConfig):
     name = 'SimpleMail'
