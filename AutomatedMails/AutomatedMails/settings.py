873dae4561ac3dfa09e519889885dd0e1d8d0f70
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-27 23:05:14 +0530

    added media files access configuartion

--- AutomatedMails/AutomatedMails/settings.py
+++ AutomatedMails/AutomatedMails/settings.py
@@ -31,12 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'SimpleMail.apps.SimpleMailConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -77,8 +79,16 @@ WSGI_APPLICATION = 'AutomatedMails.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'automatedmails',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command':"SET sql_mode= 'STRICT_TRANS_TABLES' ",
+            'charset':'utf8'
+        }
     }
 }
 
@@ -125,6 +135,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
