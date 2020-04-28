16bba9c66a65ae8c6b725e9863e8a79a7ab61ad4
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-28 16:18:23 +0530

    Updated urls

--- AutomatedMails/SimpleMail/urls.py
+++ AutomatedMails/SimpleMail/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('',views.SimpleMail,name=""),
+	path('getContacts',views.getContacts,name="getContacts"),
 	path('SendMail',views.SendSimpleEmail,name="Mail"),
 ]
