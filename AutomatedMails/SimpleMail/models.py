bd13cff1980b9385a3525f729a40fbfa5f71ac2d
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-28 16:16:53 +0530

    updated model.py

--- AutomatedMails/SimpleMail/models.py
+++ AutomatedMails/SimpleMail/models.py
@@ -6,5 +6,7 @@ from django.db import models
 class ContactDetail(models.Model):
 	Name =  models.CharField(max_length=100)
 	EmailId = models.EmailField()
+	def __str__(self):
+		return '%s %s' % (self.Name, self.EmailId)
 
 		
