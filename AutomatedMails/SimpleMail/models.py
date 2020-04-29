00d1cfe2d71b69170943e7d7744c95c3956c25d1
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-04-29 16:04:01 +0530

    Updated urls and model script files

--- AutomatedMails/SimpleMail/models.py
+++ AutomatedMails/SimpleMail/models.py
@@ -6,7 +6,5 @@ from django.db import models
 class ContactDetail(models.Model):
 	Name =  models.CharField(max_length=100)
 	EmailId = models.EmailField()
-	def __str__(self):
-		return '%s %s' % (self.Name, self.EmailId)
 
 		
--- AutomatedMails/SimpleMail/urls.py
+++ AutomatedMails/SimpleMail/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('',views.SimpleMail,name=""),
+	path('SimpleMail',views.SimpleMail,name="SimpleMail"),
 	path('getContacts',views.getContacts,name="getContacts"),
-	path('SendMail',views.SendSimpleEmail,name="Mail"),
+	path('SendMail',views.SendEmail,name="SendMail"),
+	path('BulkMail',views.BulkMail,name="BulkMail")
 ]
